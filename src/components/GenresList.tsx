e47d79356407ea102ed969a3a7d4031713d7e5be
Alexander-logikkz <alexander.osman@logikkz.com>
2023-11-06 12:50:01 +0000

    chore: Displaying genres

--- src/components/GenresList.tsx
+++ src/components/GenresList.tsx
@@ -1,16 +1,21 @@
+import { HStack, Image, List, ListItem, Text } from "@chakra-ui/react";
 import useGenres from "../hooks/useGenres";
+import getCroppedImageUrl from "../services/imageUrl";
 
 const GenresList = () => {
   const { data } = useGenres();
 
   return (
-    <div>
-      <ul>
-        {data.map((genre) => (
-          <li key={genre.id}>{genre.name}</li>
-        ))}
-      </ul>
-    </div>
+    <List>
+      {data.map((genre) => (
+        <ListItem key={genre.id} paddingY='5px'>
+          <HStack>
+            <Image boxSize='32px' borderRadius={8} src={getCroppedImageUrl(genre.image_background)}/>
+            <Text>{genre.name}</Text>
+          </HStack>
+        </ListItem>
+      ))}
+    </List>
   );
 };
 
