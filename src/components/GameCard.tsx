3b672663be5916345b67dacaedae5349372a48b8
Alexander-logikkz <alexander.osman@logikkz.com>
2023-11-06 23:30:11 +0000

    chore: Clean up the game cards

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -13,13 +13,13 @@ const GameCard = ({ game }: Props) => {
     <Card>
       <Image src={getCroppedImageUrl(game.background_image)} />
       <CardBody>
-        <Heading fontSize="2xl">{game.name}</Heading>
-        <HStack justifyContent="space-between">
+        <HStack justifyContent="space-between" marginBottom={3}>
           <PlatformIconList
             platform={game.platforms.map(({ platform }) => platform)}
           />
           <MetacriticScore metacritic={game.metacritic} />
         </HStack>
+        <Heading fontSize="2xl">{game.name}</Heading>
       </CardBody>
     </Card>
   );
