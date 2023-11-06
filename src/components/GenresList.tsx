0de71d46813de558c9c8f49d72e11b5ed114198b
Alexander-logikkz <alexander.osman@logikkz.com>
2023-11-06 12:53:38 +0000

    chore: Showing spinner while fetching genres

--- src/components/GenresList.tsx
+++ src/components/GenresList.tsx
@@ -1,16 +1,24 @@
-import { HStack, Image, List, ListItem, Text } from "@chakra-ui/react";
+import { HStack, Image, List, ListItem, Spinner, Text } from "@chakra-ui/react";
 import useGenres from "../hooks/useGenres";
 import getCroppedImageUrl from "../services/imageUrl";
 
 const GenresList = () => {
-  const { data } = useGenres();
+  const { data, loading, error } = useGenres();
+
+  if (error) return null;
+
+  if (loading) return <Spinner />;
 
   return (
     <List>
       {data.map((genre) => (
-        <ListItem key={genre.id} paddingY='5px'>
+        <ListItem key={genre.id} paddingY="5px">
           <HStack>
-            <Image boxSize='32px' borderRadius={8} src={getCroppedImageUrl(genre.image_background)}/>
+            <Image
+              boxSize="32px"
+              borderRadius={8}
+              src={getCroppedImageUrl(genre.image_background)}
+            />
             <Text>{genre.name}</Text>
           </HStack>
         </ListItem>
